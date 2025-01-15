79604094d0f0fcc5cc48903ee939d0c1fcb0c394
tirafrutas <smoralesg@uoc.edu>
2025-01-15 12:11:50 +0100

    Dynamic retrieval of properties.

--- src/extension/swdc-dsl-md-document-generator.ts
+++ src/extension/swdc-dsl-md-document-generator.ts
@@ -29,39 +29,40 @@ export class MDDocumentGenerator implements Generator {
     }
 
     generateMDfromJSON(sdc: any) : string {
-        const participantLabels = ['age', 'ethnicity', 'gender', 'socioEconomicStatus', 'skillLevel', 'tenure'];
-        const entityLabels = ['type', 'startingAgeRange', 'endingAgeRange', 'ethnicities', 'genders', 'socioEconomicStati', 'skillLevels', 'averageTenure'];
-        const teamLabels = entityLabels.concat(['startDate', 'endDate', 'teamSize', 'iterations'])
-            .concat(['salary', 'labourRights']) // LabourForce
-            .concat(['type', 'testingGuidelines', 'appMaturity']) // TesterTeam
-            .concat(['type', 'reportingMethod', 'reportingPlatform']); // PublicReporterTeam
-        const governanceLabels = ['projectType', 'governanceProcesses', 'funders', 'shareholders'];
-        const socialContextLabels = ['description','culturalTraits', 'country', 'spokenLanguages', 'relatedTeams'];
-        const useCaseLabels = ['description', 'targetCommunities'];
-        const adaptationLabels = ['description', 'release', 'useCases', 'targetCommunities', 'relatedTeams'];
+        // const participantLabels = ['age', 'ethnicity', 'gender', 'socioEconomicStatus', 'skillLevel', 'tenure'];
+        // const entityLabels = ['type', 'startingAgeRange', 'endingAgeRange', 'ethnicities', 'genders', 'socioEconomicStati', 'skillLevels', 'averageTenure'];
+        // const teamLabels = entityLabels.concat(['startDate', 'endDate', 'teamSize', 'iterations'])
+        //     .concat(['salary', 'labourRights']) // LabourForce
+        //     .concat(['type', 'testingGuidelines', 'appMaturity']) // TesterTeam
+        //     .concat(['type', 'reportingMethod', 'reportingPlatform']); // PublicReporterTeam
+        // const governanceLabels = ['projectType', 'governanceProcesses', 'funders', 'shareholders'];
+        // const socialContextLabels = ['description','culturalTraits', 'country', 'spokenLanguages', 'relatedTeams'];
+        // const useCaseLabels = ['description', 'targetCommunities'];
+        // const adaptationLabels = ['description', 'release', 'useCases', 'targetCommunities', 'relatedTeams'];
 
         let result = new Array();
         
         result.push(this.header('# Software Diversity Card'));
         result.push(this.header('## Entities and Individuals'));
-        result.push(this.generateSubsection('### Participants', sdc.participants, participantLabels)); // TODO: spoken languages
-        result.push(this.generateSubsection('### Teams', sdc.teams, teamLabels)); // TODO: team participants
-        result.push(this.generateSubsection('### Target Communities', sdc.targetCommunities, teamLabels.concat('description')));
-        result.push(this.generateSubsection('### Organizations', sdc.organizations, teamLabels));
+        result.push(this.generateSubsection('### Participants', sdc.participants)); //, participantLabels)); // TODO: spoken languages
+        result.push(this.generateSubsection('### Teams', sdc.teams)); //, teamLabels)); // TODO: team participants
+        result.push(this.generateSubsection('### Target Communities', sdc.targetCommunities)); //, teamLabels.concat('description')));
+        result.push(this.generateSubsection('### Organizations', sdc.organizations)); //, teamLabels));
         result.push(this.header('## Contexts'));
-        result.push(this.generateSubsection('### Governance', sdc.governances, governanceLabels));
-        result.push(this.generateSubsection('### Social Contexts', sdc.socialContexts, socialContextLabels));
-        result.push(this.generateSubsection('### Use Cases', sdc.useCases, useCaseLabels));
-        result.push(this.generateSubsection('### Adaptations', sdc.adaptations, adaptationLabels));
+        result.push(this.generateSubsection('### Governance', sdc.governances)); //, governanceLabels));
+        result.push(this.generateSubsection('### Social Contexts', sdc.socialContexts)); //, socialContextLabels));
+        result.push(this.generateSubsection('### Use Cases', sdc.useCases)); //, useCaseLabels));
+        result.push(this.generateSubsection('### Adaptations', sdc.adaptations)); //, adaptationLabels));
 
         return result.filter(function(element) { return element !== undefined; }).join('');
     }
 
-    generateSubsection(title: string, elements: any[], labels: string[]) : string | undefined {
+    generateSubsection(title: string, elements: any[]) : string | undefined {
         if (elements.length > 0) {
            let result = this.header(title);
             for (var elem of elements) {
-                var output = this.item(elem.id, labels.map(e => this.itemize(e, elem[e])));
+                var properties = Object.getOwnPropertyNames(elem).filter(p => p !== 'id' && p !== 'participants');
+                var output = this.item(elem.id, properties.map(p => this.itemize(p, elem[p])));
                 result = result.concat(output);
             }
             return result;
