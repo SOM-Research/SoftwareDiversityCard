3b257a72773dc5f3e611043383b2a973f9bae8e5
tirafrutas <smoralesg@uoc.edu>
2025-01-15 15:25:21 +0100

    Added generation of spoken languages and team participants. Refactored generation of labels.

--- src/extension/swdc-dsl-md-document-generator.ts
+++ src/extension/swdc-dsl-md-document-generator.ts
@@ -4,6 +4,11 @@ import { isModel } from '../language/generated/ast.js';
 import { NodeFileSystem } from 'langium/node';
 import { JSONGenerator } from './swdc-json-generator.js';
 
+enum EnumAdditionalAssociations {
+    None,
+    SpokenLanguages,
+    TeamParticipants
+};
 
 export interface Generator {
     generate(model : string | AstNode) : string | undefined;
@@ -29,40 +34,43 @@ export class MDDocumentGenerator implements Generator {
     }
 
     generateMDfromJSON(sdc: any) : string {
-        // const participantLabels = ['age', 'ethnicity', 'gender', 'socioEconomicStatus', 'skillLevel', 'tenure'];
-        // const entityLabels = ['type', 'startingAgeRange', 'endingAgeRange', 'ethnicities', 'genders', 'socioEconomicStati', 'skillLevels', 'averageTenure'];
-        // const teamLabels = entityLabels.concat(['startDate', 'endDate', 'teamSize', 'iterations'])
-        //     .concat(['salary', 'labourRights']) // LabourForce
-        //     .concat(['type', 'testingGuidelines', 'appMaturity']) // TesterTeam
-        //     .concat(['type', 'reportingMethod', 'reportingPlatform']); // PublicReporterTeam
-        // const governanceLabels = ['projectType', 'governanceProcesses', 'funders', 'shareholders'];
-        // const socialContextLabels = ['description','culturalTraits', 'country', 'spokenLanguages', 'relatedTeams'];
-        // const useCaseLabels = ['description', 'targetCommunities'];
-        // const adaptationLabels = ['description', 'release', 'useCases', 'targetCommunities', 'relatedTeams'];
-
         let result = new Array();
         
         result.push(this.header('# Software Diversity Card'));
         result.push(this.header('## Entities and Individuals'));
-        result.push(this.generateSubsection('### Participants', sdc.participants)); //, participantLabels)); // TODO: spoken languages
-        result.push(this.generateSubsection('### Teams', sdc.teams)); //, teamLabels)); // TODO: team participants
-        result.push(this.generateSubsection('### Target Communities', sdc.targetCommunities)); //, teamLabels.concat('description')));
-        result.push(this.generateSubsection('### Organizations', sdc.organizations)); //, teamLabels));
+        result.push(this.generateSubsection('### Participants', sdc.participants, EnumAdditionalAssociations.SpokenLanguages));
+        result.push(this.generateSubsection('### Teams', sdc.teams, EnumAdditionalAssociations.TeamParticipants));
+        result.push(this.generateSubsection('### Target Communities', sdc.targetCommunities));
+        result.push(this.generateSubsection('### Organizations', sdc.organizations));
         result.push(this.header('## Contexts'));
-        result.push(this.generateSubsection('### Governance', sdc.governances)); //, governanceLabels));
-        result.push(this.generateSubsection('### Social Contexts', sdc.socialContexts)); //, socialContextLabels));
-        result.push(this.generateSubsection('### Use Cases', sdc.useCases)); //, useCaseLabels));
-        result.push(this.generateSubsection('### Adaptations', sdc.adaptations)); //, adaptationLabels));
+        result.push(this.generateSubsection('### Governance', sdc.governances));
+        result.push(this.generateSubsection('### Social Contexts', sdc.socialContexts));
+        result.push(this.generateSubsection('### Use Cases', sdc.useCases));
+        result.push(this.generateSubsection('### Adaptations', sdc.adaptations));
 
         return result.filter(function(element) { return element !== undefined; }).join('');
     }
 
-    generateSubsection(title: string, elements: any[]) : string | undefined {
+    generateSubsection(title: string, elements: any[], additionalAssocs: EnumAdditionalAssociations = EnumAdditionalAssociations.None) : string | undefined {
         if (elements.length > 0) {
            let result = this.header(title);
             for (var elem of elements) {
-                var properties = Object.getOwnPropertyNames(elem).filter(p => p !== 'id' && p !== 'participants');
-                var output = this.item(elem.id, properties.map(p => this.itemize(p, elem[p])));
+                var properties = Object.getOwnPropertyNames(elem).filter(p => p !== 'id');
+                var spokenLanguages = '', teamParticipants = '';
+                if (additionalAssocs == EnumAdditionalAssociations.SpokenLanguages) {
+                    properties = properties.filter(p => p !== 'spokenLanguages');
+                    spokenLanguages = this.generateSpokenLanguages(elem['spokenLanguages']);
+                    //parsedProperties.push(spokenLanguages);
+                }
+                else if (additionalAssocs == EnumAdditionalAssociations.TeamParticipants) {
+                    properties = properties.filter(p => p !== 'participants');
+                    teamParticipants = this.generateTeamParticipants(elem['participants']);
+                    //parsedProperties.push(teamParticipants);
+                }
+                var parsedProperties = properties.map(p => this.itemize(p, elem[p]));
+                parsedProperties.push(spokenLanguages);
+                parsedProperties.push(teamParticipants);
+                var output = this.item(elem.id, parsedProperties);
                 result = result.concat(output);
             }
             return result;
@@ -70,18 +78,41 @@ export class MDDocumentGenerator implements Generator {
         return undefined;
     }
 
+    generateSpokenLanguages(spokenLanguages: any[]) : string {
+        let result = this.label('spokenLanguages').concat('<br>');
+        result = result.concat('| language | proficiency |<br>');
+        result = result.concat('| -------- | ----------- |<br>');
+        let languages = spokenLanguages.map(sl => '| '.concat(sl.language, ' | ', sl.proficiency, ' |'));
+        return result.concat(languages.join('<br>'), '<br>');
+    }
+
+    generateTeamParticipants(teamParticipants: any[]) : string {
+        let result = this.label('teamParticipants').concat('<br>');
+        result = result.concat('| participant | role | start | end |<br>');
+        result = result.concat('| ----------- | ---- | ----- | --- |<br>');
+        let participants = teamParticipants.map(tp =>
+                '| '.concat(
+                tp.participant,
+                ' | ', tp.role,
+                ' | ', tp.startingDate,
+                (tp.endingDate) ? ' | ' + tp.endingDate : '',
+                ' |'
+            ));
+        return result.concat(participants.join('<br>'), '<br>');
+    }
+
     // Auxiliary formatting methods
 
     item(name: string, elements: string[]) : string {
-        let boldName = '**' + name + '**';
-        return boldName.concat('<br>', elements.filter(function(element) { return element !== ''; }).join(''), '<br>');
+        let boldName = this.header('#### ' + name);
+        return boldName.concat(elements.filter(function(element) { return element !== ''; }).join(''), '<br>');
     }
 
     itemize(label: string, value: string[] | string) : string {
         if (value) {
             let formattedValue;
-            if (Array.isArray(value)) formattedValue = '- '.concat(label, ': ', (value as string[]).join(', '));
-            else formattedValue = '- '.concat(label, ': ', value as string);
+            if (Array.isArray(value)) formattedValue = this.label(label).concat((value as string[]).join(', '));
+            else formattedValue = this.label(label).concat(value as string);
             return formattedValue.concat('<br>');
         }
         else
@@ -90,6 +121,10 @@ export class MDDocumentGenerator implements Generator {
 
     header(str: string) : string {
         return str.concat('<br><br>');
+    }
+
+    label(str: string) : string {
+        return '- _'.concat(str, '_: ');
     }
 
 }
